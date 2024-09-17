30be36b14e9a0eaf80714a130cb94533e076174b
LilyBurton <lily.burton1992@gmail.com>
2024-09-17 15:05:41 +0100

    Negative numbers throws errors

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -7,10 +7,36 @@ public class StringCalc
 {
     internal object Add(string numbers)
     {
+        var numberString = numbers;
+
         if (String.IsNullOrEmpty(numbers)) return 0;
-        var chars = new [] {',', '\n'};
-        var result = numbers.Split(chars)
-            .Select(s => int.Parse(s))
+
+        var delimiters = new List<char> {',', '\n'};
+
+        if (numbers.StartsWith("//"))
+        {
+            var splitInput = numberString.Split('\n');
+            var newDelimiter = splitInput.First().Trim('/');
+            numberString = String.Join('\n', splitInput.Skip(1));
+
+            delimiters.Add(Convert.ToChar(newDelimiter));
+        };
+
+        var numberList = numberString.Split(delimiters.ToArray())
+            .Select(s => int.Parse(s));
+
+            var negatives = numberList
+                .Where(n => n < 0);
+
+        if (negatives.Any())
+        {
+            string negativeString = String.Join(',', negatives
+                .Select(n => n.ToString()));
+                throw new Exception($"Negatives Not Allowed: {negativeString}");
+        }
+
+        
+        var result = numberList
             .Sum();
 
         return result;
--- StringCalculator/StringCalculatorTest.cs
+++ StringCalculator/StringCalculatorTest.cs
@@ -55,4 +55,27 @@ public class StringCalculatorTest
 
         Assert.Equal(expected, result);
     }
+      
+      [Theory]
+    [InlineData("//;\n1;2;3", 6)]
+
+    public void AddThreeNumbersTogetherWithNewDelimiters(string numbers, int expected)
+    {
+        var result = _calculator.Add(numbers);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("-1, 2", "Negatives Not Allowed: -1")]
+    [InlineData("-1,-2", "Negatives Not Allowed: -1,-2")]
+
+    public void ThrowsNegativeGiven(string numbers, string expectedMessage)
+    {
+        Action action = () => _calculator.Add(numbers);
+
+        var err = Assert.Throws<Exception>(action);
+
+        Assert.Equal(expectedMessage, err.Message);
+    }
 }
